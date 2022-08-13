000d3ff3884eca96d5606ef2e1f96292956452f4
wxy@jlwh.com <wxy@jlwh.com>
2022-08-13 17:13:40 +0800

    first commit

--- app/rbac/service/internal/data/data.go
+++ app/rbac/service/internal/data/data.go
@@ -43,6 +43,7 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 		return nil, nil, err
 	}
 	// Run the auto migration tool.
+	// 自动创建表结构
 	if err := client.Schema.Create(context.Background()); err != nil {
 		helper.Errorf("failed creating schema resources: %v", err)
 		return nil, nil, err
